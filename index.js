22ae02d8d939d1b0fc98f64f010613d391fd29ec
Sarfaraz Khan <sarfarazkhan2020@hotmail.com>
2022-01-12 00:48:24 +0530

    server

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require("path"); // get method
 const app = express(); 
 // const port = 2000;
 
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Server open ')
+})
+
 // SERVER
 // app.listen(port, () => {
 //   console.log(`Server is Start on Port: ${port}`);
@@ -12,9 +18,9 @@ const app = express();
 app.use('/static',express.static('static'))
 app.use('/images',express.static('images'))
 
-const http = require("http");
-const PORT = process.env.PORT;
-const Server = http.createServer((req, res) => {
+// const http = require("http");
+// const PORT = process.env.PORT;
+// const Server = http.createServer((req, res) => {
 
 // GET METHOD
 app.get("/", (req, res) => {
@@ -41,7 +47,7 @@ app.get("/projects", (req, res) => {
   res.sendFile(path.join(__dirname + "/projects.html"));
 });
 
-});
+// });
 Server.listen(PORT,  () => {
   console.log(`server is open on heroku)`);
 });
